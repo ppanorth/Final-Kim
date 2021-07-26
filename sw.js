66cf5662e278dc47acf4539669399658ad844b74
ppanorth <80310698+ppanorth@users.noreply.github.com>
2021-07-26 10:22:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,6 +26,13 @@ Please be reminded that the submission of the following reports will be on or be
        ]
       
     };
-    e.waitUntil(self.registration.showNotification('URGENT PLEASE AHAHAHAHAHHAHAHAHHAHAHAHAHAHAHAHAHHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAS', options));
+    e.waitUntil(self.registration.showNotification('Good day!
+Please be reminded that the submission of the following reports will be on or before May 5, 2021. 
+1. Monthly Status Report on Leases  (In-charge Ma'am Aida )
+2. Consolidated ARTA Compliance Monitoring Report (In-charge Jane)
+3. Operational Summary Report of Private Ports (In-charge Sir Rey)
+4. Compliance Monitoring Report of Private Ports (In-charge Sir Rey)
+5. Register of Common Permits (In-charge Jessica)
+        ', options));
  });
  
