ac3d5cb06791fbb93a92d2fb3d8266e539ca19a9
ppanorth <80310698+ppanorth@users.noreply.github.com>
2021-07-26 10:21:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 self.addEventListener('push', function (e)  {
     var options ={
-      body: 'PPA MONITORING APP ahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha',
+      body: 'Good day!
+
+Please be reminded that the submission of the following reports will be on or before May 5, 2021. 
+
+1. Monthly Status Report on Leases  (In-charge Ma'am Aida )
+2. Consolidated ARTA Compliance Monitoring Report (In-charge Jane)
+3. Operational Summary Report of Private Ports (In-charge Sir Rey)
+4. Compliance Monitoring Report of Private Ports (In-charge Sir Rey)
+5. Register of Common Permits (In-charge Jessica)
+        ',
       //icon: 'images/example.png',
       vibrate: [100, 50, 100],
       data: {
@@ -10,7 +19,7 @@ self.addEventListener('push', function (e)  {
       actions: [
         {
           action: 'explore',
-          title: 'URGENT! Please Comply! ',
+          title: 'URGENT! ',
           //icon: 'images/checkmark.png'
         },
         { action: 'close', title: 'Close'}
